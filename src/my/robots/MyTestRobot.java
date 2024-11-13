a3dc32e4e111c25e4d3b08753c24e970a49f6d24
s-nakao <shogo.nakao@crezit.jp>
2024-11-13 12:36:08 +0900

    first commit

---
+++ src/my/robots/MyTestRobot.java
@@ -0,0 +1,77 @@
+package my.robots;
+
+import robocode.AdvancedRobot;
+import robocode.HitByBulletEvent;
+import robocode.RobocodeFileOutputStream;
+import robocode.ScannedRobotEvent;
+
+import java.io.IOException;
+
+public class MyTestRobot extends AdvancedRobot {
+
+    private int attackCount = 0;
+    private double energy = 100;
+
+
+    public void run() {
+        while (true) {
+            try {
+                RobocodeFileOutputStream robocodeFileOutputStream =
+                        new RobocodeFileOutputStream(getDataDirectory().toPath() + "/test.log", true);
+                // X,Y,Energy,Attacks
+                robocodeFileOutputStream.write(
+                        String.format("%d\t%.2f\t%.2f\t%.2f\t%d\n",
+                                getTime(),
+                                getX(),
+                                getY(),
+                                getEnergy(),
+                                attackCount
+                        ).getBytes()
+                );
+                ahead(100); // Move ahead 100
+                robocodeFileOutputStream.write(
+                        String.format("%d\t%.2f\t%.2f\t%.2f\t%d\n",
+                                getTime(),
+                                getX(),
+                                getY(),
+                                getEnergy(),
+                                attackCount
+                        ).getBytes()
+                );
+                turnGunRight(360); // Spin gun around
+                back(100); // Move back 100
+                robocodeFileOutputStream.write(
+                        String.format("%d\t%.2f\t%.2f\t%.2f\t%d\n",
+                                getTime(),
+                                getX(),
+                                getY(),
+                                getEnergy(),
+                                attackCount
+                        ).getBytes()
+                );
+                turnGunRight(360); // Spin gun around
+                robocodeFileOutputStream.flush();
+                robocodeFileOutputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    /**
+     * Fire when we see a robot
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+        attackCount++;
+    }
+
+    /**
+     * We were hit!  Turn perpendicular to the bullet,
+     * so our seesaw might avoid a future shot.
+     */
+    public void onHitByBullet(HitByBulletEvent e) {
+        energy -= e.getPower();
+        turnLeft(90 - e.getBearing());
+    }
+}
